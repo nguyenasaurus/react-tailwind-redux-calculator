3eeb06d71c451e10084deb04dd1df7d8abf337fe
nguyenasaurus <justinexnguyen@gmail.com>
2023-01-09 20:10:07 +0000

    wip: setup for theme colors with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,35 @@
 /** @type {import('tailwindcss').Config} */
+
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`
+    }
+    return `rgb(var(${variableName}))`
+  }
+}
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        theme: {
+          fill: withOpacity('--color-fill'),
+        }
+      },
+      borderColor: {
+        theme: {}
+      },
+      textColor: {
+        theme: {
+          base: withOpacity('--color-text-base'),
+          inverted: withOpacity('--color-text-inverted'),
+        }
+      }
+    },
   },
   plugins: [],
 }
