c808c0169f93405090612045ed0f117273427f76
nguyenasaurus <justinexnguyen@gmail.com>
2023-01-09 20:09:42 +0000

    refactor: drop unused imports

--- src/components/ThemeChooser.jsx
+++ src/components/ThemeChooser.jsx
@@ -1,25 +1,27 @@
-import React, {useEffect, useState} from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { themeChooser } from '../redux/themeSlice';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { themeChooser } from "../redux/themeSlice";
 
 function ThemeChooser() {
-  const theme = useSelector(state => state.theme.themeName)
   const [themeVal, setThemeVal] = useState(1);
   const dispatch = useDispatch();
 
-  const handleThemeChange = e => {
+  const handleThemeChange = (e) => {
     setThemeVal(e.target.value);
-    dispatch(themeChooser(Number(e.target.value)))
+    dispatch(themeChooser(Number(e.target.value)));
   };
 
-  useEffect(() => {
-    console.log(theme)
-  }, [theme])
-  
   return (
     <nav className="flex">
-      <h3 className="text-sm mr-3 text-white">Theme</h3>
-      <input onChange={handleThemeChange} type="range" min="1" max="3" value={themeVal} step="1" />
+      <h3 className="text-sm mr-3 text-theme-base">Theme</h3>
+      <input
+        onChange={handleThemeChange}
+        type="range"
+        min="1"
+        max="3"
+        value={themeVal}
+        step="1"
+      />
     </nav>
   );
 }
