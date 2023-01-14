509d23996720e58f9daaf777c445fb08e3d2969f
nguyenasaurus <justinexnguyen@gmail.com>
2023-01-14 21:17:25 +0000

    feat: add theme color for buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,15 +18,22 @@ module.exports = {
       backgroundColor: {
         theme: {
           fill: withOpacity('--color-fill'),
+          'button-primary': withOpacity('--color-button-primary-fill'),
+          'button-secondary': withOpacity('--color-button-secondary-fill'),
+          'button-tertiary': withOpacity('--color-button-tertiary-fill')
         }
       },
       borderColor: {
-        theme: {}
+        theme: {
+          'button-primary': withOpacity('--color-button-primary-border'),
+          'button-secondary': withOpacity('--color-button-secondary-border'),
+          'button-tertiary': withOpacity('--color-button-tertiary-border')
+        }
       },
       textColor: {
         theme: {
-          base: withOpacity('--color-text-base'),
-          inverted: withOpacity('--color-text-inverted'),
+          primary: withOpacity('--color-text-primary'),
+          'button-primary': ('--color-button-primary-text'),
         }
       }
     },
